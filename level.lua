5a81b909f9eec7d1f55eb7bd78c3e054a34ffd29
sefe <sebastian.ferngren@gmail.com>
2024-02-25 13:22:08 +0100

    add separate code file for editing in vscode

---
+++ level.lua
@@ -0,0 +1,33 @@
+
+function upd_tiles()
+    tile_borders()
+end
+
+function tile_borders()
+ cmapsig(function(x,y,sig)
+    -- if sig_match(sig, 0b10101111,0b10001001) then
+    local sld = chk_solid(p(x,y))
+    if sig_match(sig, 0b10101111,0b10001001) and not sld then
+        mset(x,y,146) --right
+    elseif sig_match(sig, 0b11011111,0b10001111) and sld then
+        mset(x,y,144) --left
+    elseif sig_match(sig, 0b01011111,0b00010011) and not sld then
+        mset(x,y,145) --bottom
+    end
+ end)
+end
+
+function cmapsig(func)
+    local sigs = {}
+	for y=0,_size do
+		for x=0,_size do
+            sigs[ptoix(x,y)] = tile_sig(p(x,y))
+		end
+	end
+
+	for y=0,_size do
+		for x=0,_size do
+			func(x,y,sigs[ptoix(x,y)])
+		end
+	end
+end
