570b301d94b95278d997cede2e04b2ae18e6b0b3
sefe <sebastian.ferngren@gmail.com>
2024-02-25 13:49:20 +0100

    fix top left corners

--- level.lua
+++ level.lua
@@ -7,12 +7,14 @@ function tile_borders()
  cmapsig(function(x,y,sig)
     -- if sig_match(sig, 0b10101111,0b10001001) then
     local sld = chk_solid(p(x,y))
-    if sig_match(sig, 0b10101111,0b10001001) and not sld then
+    if sig_match(sig, 0b10101111,0b10001101) and not sld then
         mset(x,y,146) --right
     elseif sig_match(sig, 0b11011111,0b10001111) and sld then
         mset(x,y,144) --left
     elseif sig_match(sig, 0b01011111,0b00010011) and not sld then
         mset(x,y,145) --bottom
+    elseif sig_match(sig, 0b11001111,0b11001101) and not sld then
+        mset(x,y,161) --top left corner
     end
  end)
 end
