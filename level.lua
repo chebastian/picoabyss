cec3b20984bd63057b6892351956eec318306776
sefe <sebastian.ferngren@gmail.com>
2024-02-25 19:49:20 +0100

    more corners

--- level.lua
+++ level.lua
@@ -9,12 +9,22 @@ function tile_borders()
     local sld = chk_solid(p(x,y))
     if sig_match(sig, 0b10101111,0b10001101) and not sld then
         mset(x,y,146) --right
-    elseif sig_match(sig, 0b11011111,0b10001111) and sld then
+    elseif sig_match(sig, 0b11011111,0b11001111) and sld then
         mset(x,y,144) --left
-    elseif sig_match(sig, 0b01011111,0b00010011) and not sld then
+    elseif sig_match(sig, 0b00011111,0b0001101) and not sld then
+        mset(x,y,147) --top
+    elseif sig_match(sig, 0b11101100,0b11001100) and sld then
         mset(x,y,145) --bottom
     elseif sig_match(sig, 0b11001111,0b11001101) and not sld then
         mset(x,y,161) --top left corner
+    elseif sig_match(sig, 0b01111101,0b01101101) and not sld then
+        mset(x,y,163) --top right corner
+    elseif sig_match(sig, 0b11101101,0b11001101) and not sld then
+        mset(x,y,160) --bottom left corner
+    elseif sig_match(sig, 0b11001101,0b11001101) and sld then
+        mset(x,y,162) --bottom right corner (!done)
+    elseif sig_match(sig, 0b11111111,0b11001111) and sld then
+        mset(x,y,132) --solid not (correct?)
     end
  end)
 end
