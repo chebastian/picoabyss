61ca9492b8bacb862ca5caa7c6832db8cb415860
sefe <sebastian.ferngren@gmail.com>
2024-02-25 21:06:44 +0100

    corners bugfix

--- level.lua
+++ level.lua
@@ -8,23 +8,24 @@ function tile_borders()
     -- if sig_match(sig, 0b10101111,0b10001001) then
     local sld = chk_solid(p(x,y))
     if sig_match(sig, 0b10101111,0b10001101) and not sld then
-        mset(x,y,146) --right
+        mset(x,y,146) --left
+    -- elseif sig_match(sig, 0b11011111,0b11001111) and sld then
     elseif sig_match(sig, 0b11011111,0b11001111) and sld then
-        mset(x,y,144) --left
-    elseif sig_match(sig, 0b00011111,0b0001101) and not sld then
+        mset(x,y,144) --right
+    elseif sig_match(sig, 0b01011111,0b01001101) and not sld then
         mset(x,y,147) --top
     elseif sig_match(sig, 0b11101100,0b11001100) and sld then
         mset(x,y,145) --bottom
     elseif sig_match(sig, 0b11001111,0b11001101) and not sld then
         mset(x,y,161) --top left corner
-    elseif sig_match(sig, 0b01111101,0b01101101) and not sld then
-        mset(x,y,163) --top right corner
-    elseif sig_match(sig, 0b11101101,0b11001101) and not sld then
+    elseif sig_match(sig, 0b00011101,0b01101101) and not sld then
+        mset(x,y,163) --top right corner (incorrect)
+    elseif sig_match(sig, 0b11101101,0b11001101)  and not sld then
         mset(x,y,160) --bottom left corner
-    elseif sig_match(sig, 0b11001101,0b11001101) and sld then
+    elseif sig_match(sig, 0b11001101,0b11001111) and sld then
         mset(x,y,162) --bottom right corner (!done)
-    elseif sig_match(sig, 0b11111111,0b11001111) and sld then
-        mset(x,y,132) --solid not (correct?)
+    -- elseif sig_match(sig, 0b11111111,0b11001111) and sld then
+    --     mset(x,y,132) --solid not (correct?)
     end
  end)
 end
