807401dd26649ed518313f96a6a44eda1ffd61fc
sefe <sebastian.ferngren@gmail.com>
2024-02-26 06:41:25 +0100

    remove commented out lines

--- level.lua
+++ level.lua
@@ -5,11 +5,9 @@ end
 
 function tile_borders()
  cmapsig(function(x,y,sig)
-    -- if sig_match(sig, 0b10101111,0b10001001) then
     local sld = chk_solid(p(x,y))
     if sig_match(sig, 0b10101111,0b10001101) and not sld then
         mset(x,y,146) --left
-    -- elseif sig_match(sig, 0b11011111,0b11001111) and sld then
     elseif sig_match(sig, 0b11011111,0b11001111) and sld then
         mset(x,y,144) --right
     elseif sig_match(sig, 0b01011111,0b01001101) and not sld then
