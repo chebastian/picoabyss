04a4e85fcf07ea6feb9cc4bf23e0bbc8a1293590
sefe <sebastian.ferngren@gmail.com>
2024-02-28 07:09:15 +0100

    move gen to separate file

---
+++ gen.lua
@@ -0,0 +1,629 @@
+-- gen
+
+function go_to_o(lookup,pos,ocp)
+ local cp,cd = min_d_on_map(lookup,pos,ocp)
+ if not cp then
+  return
+ end
+ local path = {p(pos.x+cp.x,pos.y+cp.y)}
+ local nxtp = path[1]
+ while cd > 0 do
+ 	cp,cd = min_d_on_map(lookup,nxtp,ocp)
+ 	local np = p(nxtp.x+cp.x,nxtp.y+cp.y)
+ 	add(path,p(np.x,np.y))
+ 	nxtp = np
+ end
+ 
+ return path
+end
+
+function min_d_on_map(lookup,pos,ocp)
+-- local lookup = arr_to_tbl(nodes)
+ local mini,mind = -1,999
+ local curd = lookup[ptoi(pos)]
+ 
+ -- if we are on map our min is our current dist
+ if curd then
+  mind = curd
+ end
+ 
+ for i=1,#dirs do
+  local nx = pos.x+dirs[i].x
+  local ny = pos.y+dirs[i].y
+  
+  local ndist = 999
+  local look_d = lookup[ptoix(nx,ny)]
+  -- if lookd_d is on path  
+  -- and aint occupied
+  -- and is new low
+  if look_d 
+  and ocp[ptoix(nx,ny)] == nil
+  and look_d < mind
+  then
+			mini = i
+			mind = look_d
+  end
+ end
+ 
+ if mini == -1 then
+  return
+ end
+ 
+ return dirs[mini],mind
+end
+
+function path_between(_a,_b,ocp)
+	local dpth,queue,visited = 0,{},{}
+ visited[ptoi(_a)] = true
+ local nxt = {{po=_a,dst=0}}
+ local found = {{po=_a,dst=0}}
+ local goal_rch = false
+ while not goal_rch do
+ 	dpth+=1
+ 	_dbg[2] = "t:"..dpth
+		 _dbg[4] = "nxt: "..#nxt
+	 for ite in all(nxt) do
+	 	
+		 for d in all(dirs) do
+		 	local nxtp= ite.po
+		  local np = p(nxtp.x+d.x
+		  											,nxtp.y+d.y)
+		  local pi = ptoi(np)
+		  if chk_solid(np) == false
+					  and ocp[pi] == nil
+					  and visited[pi] == nil
+		  then
+		   visited[ptoi(np)] = true
+		   add(queue,{po=p(np.x,np.y),dst=dpth})
+		  end
+		 end
+	 end
+	 
+	 nxt = {}
+	 for q in all(queue) do
+	  add(nxt,
+	  {po=p(q.po.x,q.po.y),
+	   dst=q.dst})
+	  add(found,
+	   {po=p(q.po.x,q.po.y),
+	    dst=q.dst})
+	  if (q.po.x == _b.x 
+	  and q.po.y == _b.y) then
+	  	_dbg[5] = "✽ound" .. #found
+	   return found
+	  end
+	 end
+	 	 
+	 if #queue == 0 then
+	  _dbg[5] = "not found"
+
+	  return found
+	 end
+	 
+	 queue = {}
+
+ end
+ return found
+end
+
+-- generate
+
+function add_door(r,hr,existing)
+ local ranx = rnd()
+ local np = {}
+ if not hr then
+     np =
+         p(in_rng(r.x+flr(ranx*r.w),r.x+1,r.x+r.w-1),
+     									r.y+r.h)
+    else
+      np = p(r.x+r.w,
+           in_rng(r.y+flr(ranx*r.h),
+                  r.y+1,r.y+r.h-1))
+    end
+ existing[ptoi(np)] = np
+end
+
+function regen(iter,sz)
+--	 reload(0x1000, 0x1000, 0x2000)
+  local w,h = sz,sz
+  local rc = rct(0,0,w,h)
+  local iters = iter
+  local rs = {rc}	
+  local cnt = 0
+  _ps = {}
+  while iters > 0 do
+  	local nxt = {}
+   for ir in all(rs) do
+    local hr = iters%2==0
+    local valid = false
+    local retry = 10
+    local nl,nr = {},{}
+    while(not valid) do
+	    nl,nr = spl_rct(ir,hr)
+	    if not hr then
+	     local a,b = p(nl.x,nl.y+nl.h),
+	                 p(nl.x+nl.w,nl.y+nl.h)
+	     valid = _ps[ptoi(a)] == nil and _ps[ptoi(b)] == nil
+	    else
+	      local a,b = p(nl.x+nl.w,nl.y),
+	                 p(nl.x+nl.w,nl.y+nl.h)
+	     valid = _ps[ptoi(a)] == nil and _ps[ptoi(b)] == nil
+	    end
+	    retry -= 1
+	    if retry < 0 then
+	     break
+	    end
+    end
+    cnt += 1
+    add_door(nl,hr,_ps)
+    add(nxt,nl)
+    add(nxt,nr)
+   end
+   
+   rs = {}
+   for n in all(nxt) do
+   	add(rs,n)
+   end
+   iters-=1
+  end
+  
+  return rs
+end
+
+-- utils
+
+function in_rng(n,_min,_max)
+ return min(_max,max(_min,n))
+end
+
+-- rect
+
+function rct(x,y,w,h)
+ return {x=x,y=y,w=w,h=h}
+end
+
+function spl_rct(r,hor)
+	local x,y,w,h = r.x,r.y,r.w,r.h
+ 
+ local sp = in_rng(rnd(),.34,.66)
+ local nw,nh = flr(w*sp),
+ 														flr(h*sp)
+ if hor then
+  return rct(x,y,nw,h),
+  							rct(x+nw,y,w-nw,h)
+ else
+  return rct(x,y,w,nh),
+  							rct(x,y+nh,
+  							w,h-nh)
+ end
+end
+
+-- map things to tilemap
+function map_rct(r)
+ local x,y,w,h = r.x,r.y,r.w,r.h
+ local idx = 1
+ local flrid = 2+_room_idx
+ for i=0,w,1 do
+  mset(x+i,y,idx)
+  mset(x+i,y+h,idx)
+ end
+ 
+ for i=0,h,1 do
+  mset(x,y+i,idx)
+  mset(x+w,y+i,idx)
+  if i != 0 and i != h then
+	  for mx=1,w-1,1 do
+	  	mset(x+mx,y+i,flrid)
+	  end
+  end
+
+ end
+ 
+ _room_idx+=1
+ _room_idx%=5
+end
+
+function map_rct_rnd(r)
+-- ★ hack to never gen rooms of size 1
+ local nw =	rnd_rng(r.w/2,r.w)
+ local nh =	rnd_rng(r.h/2,r.h)
+ local nx = r.x
+ local x,y,w,h = nx,r.y,
+ 																nw,nh
+ local idx = 1
+ local flrid = 2+_room_idx
+ for i=0,w,1 do
+  mset(x+i,y,idx)
+  mset(x+i,y+h,idx)
+ end
+ 
+ for i=0,h,1 do
+  mset(x,y+i,idx)
+  mset(x+w,y+i,idx)
+  if i != 0 and i != h then
+	  for mx=1,w-1,1 do
+	  	mset(x+mx,y+i,flrid)
+	  end
+  end
+
+ end
+ 
+ _room_idx+=1
+ _room_idx%=5
+end
+
+function map_doors()
+ for k,v in pairs(_ps) do
+  mset(v.x,v.y,9)
+ end
+end
+
+-- 
+-- ● gen maze
+--
+
+-- gen and dig
+function init_gen()
+	sig_dir = {
+	0b11111111,
+	0b01110110, -- left
+	0b10110011, -- up
+	0b11011001, -- right
+	0b11101100, -- down
+	}
+	
+	sig_msk = {
+	0b00000000,
+	0b00001001,
+	0b00001100,
+	0b00000110,
+	0b00000011,
+	}
+	
+	_iterations = 3
+	_size = 30
+	sig_dig = 255
+	t_dig = 49
+	t_ndig = 1
+end
+
+
+function update_digables()
+	_digable = {}
+	mapsig(function(x,y,sig)
+								 if chk_solid(p(x,y)) and sig == sig_dig then
+											mset(x,y,t_dig)
+											add(_digable,{x=x,y=y})
+									end								
+								end)
+end
+
+function is_carvable(a)
+	for i=1,#sig_dir do
+	 if sig_match(a,sig_dir[i],sig_msk[i]) then
+	 	return true
+	 end
+	end
+	return false
+end
+
+function dig(po)
+ local px,py,dx,dy = po.x,po.y,0,1
+	mset(px,py,0)
+	local nextdig = {}
+	for dr in all(dirs) do
+		if not chk_solid(po) and is_carvable(tile_sig(add_t(po,dr))) then
+			add(nextdig,add_t(po,dr))
+		end
+	end	
+	
+	return nextdig
+end
+
+function dig_tunnel()
+ local start = arr_choose(_digable)
+ local ndig = dig(start)
+ local keepdigging = #ndig > 0
+
+	local dug = {}
+ while keepdigging do
+		local pdig = arr_choose(ndig)
+		if dug[ptoi(pdig)] then
+			printh("backtracking lets break")
+			break
+		end
+		dug[ptoi(pdig)] = true
+ 	ndig = dig(pdig)
+
+ 	_curx=pdig.x*8
+ 	_cury=pdig.y*8
+ 	keepdigging = #ndig > 0 	
+ end
+end
+
+function gen()
+	init_gen()
+ fill_map(1)
+ _gen_rct = regen(_iterations,_size)	
+ _room_idx = 0
+ foreach(_gen_rct,map_rct_rnd)
+
+-- set tiles digable when
+-- sourounded by walls
+	set_digable_start()
+	
+	update_digables()
+	while #_digable>1 do
+		local st = dig_tunnel()
+		update_digables()
+	end
+	
+ set_digable_start()
+ 
+end
+
+function set_digable_start()
+	mapsig(function(x,y,sig)
+								 if chk_solid(p(x,y)) then
+									 if is_digable(sig) then
+									 	mset(x,y,t_dig)
+									 	add(digs,p(x,y))
+									 else
+									 	mset(x,y,t_ndig)
+									 end
+								 end
+							end)
+end
+
+function is_digable(a)
+ if sig_match(a,255) then
+ 	return true
+ end
+	return false
+end
+
+-- utils for rnd
+
+function rnd_idx(n)
+	return flr(rnd()*n) + 1
+end
+
+function arr_choose(myarr)
+	local mri = rnd_idx(#myarr)
+	return myarr[mri]
+end
+
+function mapsig(func)
+	for y=0,_size do
+		for x=0,_size do
+			func(x,y,tile_sig(p(x,y)))
+		end
+	end
+end
+
+function fill_map(t)
+	mapsig(function(x,y,sig)
+		mset(x,y,t)
+		end)
+end
+
+function flag_map()
+ _cf = 0
+ local mapf = {}
+ mapsig(
+ function(x,y,sig)
+ 		local idx = ptoix(x,y)
+   if mapf[idx] then
+   elseif not chk_solid(p(x,y)) then
+   	flag_section(x,y,_cf,mapf)
+   	_cf+=1
+   end
+ end
+ )
+ 
+ _flagmap = {}
+ _flagmap = mapf
+ _dbg[3] = #_flagmap
+ _dbg[4] = _cf
+end
+
+function flag_section(x,y,f,res)
+	local flooded = flood_fill(p(x,y),{})
+	for po in all(flooded) do
+--		add(res,{po=po.po, f=f})
+		res[ptoi(po.po)] = {po=po.po, f=f}
+	end
+--	stop()
+end
+
+-- signature funcs
+
+function tile_sig(po)
+ local sig = 0
+ for i=1,8 do
+  local d = dir8[i]
+  if chk_solid(add_t(po,d)) then
+   sig += 1
+  end
+  if i < 8 then
+  	sig = shl(sig,1)
+  end
+ end
+ return sig
+end
+
+function inbound(x,y)
+	return x > 0 and x < _size and y > 0 and y < _size
+end
+
+function gen_junctions()
+	_junctions = {}
+ mapsig(
+	 function(x,y,sig)  
+		 if inbound(x,y-1)
+		 and inbound(x,y+1)
+		 and  sig_match(sig,0b10100000,
+		 																			0b00001111) 
+		 and chk_solid(p(x,y))
+			then
+		 	add(_junctions,
+		 	{
+		 		x=x,
+		 		y=y,
+		 		a=_flagmap[ptoix(x,y-1)].f,
+		 		b=_flagmap[ptoix(x,y+1)].f
+	 		})
+		 elseif inbound(x-1,y)
+		 and inbound(x+1,y)
+		 and sig_match(sig,0b01010000,
+		 																		0b00001111)
+			and chk_solid(p(x,y))
+			then
+			 add(_junctions,
+			 	{
+			 		x=x,
+			 		y=y,
+			 		a=_flagmap[ptoix(x-1,y)].f,
+			 		b=_flagmap[ptoix(x+1,y)].f
+		 		})
+		 end 
+	 end
+ )
+end
+
+function merge_small_areas()
+	for i=0,_cf do
+  local cons = {}
+ 	local p = get_passage(_junctions,i)
+ 	if(#p > 0) then
+	  for co in all(p) do
+	  	if not cons[co.v] then
+	  		cons[co.v] = true
+	  		local minsz = 12
+	  		if section_sz(co.v) <= minsz
+	  				 and section_sz(i) <= minsz
+	  	 then
+	  			mset(co.x,co.y,48)
+	  		end
+	  	end
+	  end 
+ 	end
+	end
+end
+
+function merge_areas()
+ 
+ gen_junctions()
+-- merge_small_areas()
+ flag_map()
+ gen_junctions()
+ -- 
+ -- add door to random passage
+ -- 
+ for i=0,_cf do
+  local cons = {}
+
+ 	local p = get_passage(_junctions,i)
+ 	if(#p > 0) then
+	 	local opn = arr_choose(p)
+	  for co in all(p) do
+	  	if not cons[co.v] then
+	  		cons[co.v] = true
+--	  		mset(co.x,co.y,9)
+					upd_door_tiles(co.x,co.y)
+	  	end
+	  end 
+ 	end
+	end
+end
+
+
+ofset = {}
+function upd_door_tiles(x,y)
+	if ofset[ptoix(x,y)] then
+--		stop("cao")
+		return
+	end
+	offset_tile(x,y,5)
+	ofset[ptoix(x,y)] = true
+	offset_tile(x-1,y,5)
+	offset_tile(x,y-1,5)
+	offset_tile(x-1,y-1,5)
+end
+
+function offset_tile(x,y,of)
+	local tk = mget(x,y)
+--	stop()
+	mset(x,y,tk+of)
+end
+
+function section_sz(idx)
+	cnt = 0
+	for k,v in pairs(_flagmap) do
+		cnt = v.f == idx and cnt+1 or cnt
+	end
+	
+	return cnt
+end
+
+function get_passage(juncs,to)
+	local res = {}
+	for j in all(juncs) do
+		if j.a != j.b and (j.a == to or j.b == to) then
+			local other = j.a == to and j.b or j.a
+			add(res,{x=j.x,y=j.y,v=other})
+		end
+	end
+	return res
+end
+
+function add_slimes()
+	mapsig(function(x,y,sig)
+		local r1,r2,r3 = rnd(1000),
+																		rnd(1000),
+																		rnd(1000)
+		if not chk_solid(p(x,y)) then
+			if     gen_traps(x,y,sig,r1) then
+			elseif gen_tiles(x,y,sig,r2) then
+			elseif gen_mobs(x,y,sig, r3) then
+			end
+		end
+	 
+	end)
+end
+
+function gen_traps(x,y,sig,r)
+	if not chk_solid(p(x,y)) then
+		if sig == 0 and r <= 100 then
+		 add_trap(_mobacid,p(x,y))
+	 	return true
+	 end
+ end
+	return false
+end
+
+function gen_tiles(x,y,sig,r)
+ if _genweed.pred(sig)
+	and r <= _genweed.r
+	then
+		mset(x,y,_idweed)
+		return true
+	elseif r < _genclam.r then
+		mset(x,y,_idclam)
+		return true
+	end
+	
+	return false
+end
+
+function gen_mobs(x,y,sig,r)
+ if r <= _gensqid.r  then
+	 add_mob(_gensqid.id,p(x,y))
+ 	return true
+ elseif r <= _gensnek.r then
+ 	add_mob(_gensnek.id,p(x,y))
+ 	return true
+ end
+ 
+ return true
+end
